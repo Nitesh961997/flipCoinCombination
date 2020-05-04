a4534b8395588e01b365a52bb9a65cbde573ecf6
Nitesh961997 <niteshkavathankar@gmail.com>
2020-05-04 12:10:18 +0530

    percentage of heads and tails is shown

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,8 +1,23 @@
 #!/bin/bash -x
-result=$((RANDOM%2))
-if [ $result -eq 1 ]
-	then
-		echo heads is winner
-	else
-		echo tails is winner
-fi
+heads=0
+tails=0
+toss=0
+for (( i=1; i<=100; i++))
+do
+	result=$((RANDOM%2))
+	((toss++))
+	if [ $result -eq 1 ]
+		then
+			echo heads is winner
+			((heads++))
+		else
+			echo tails is winner
+			((tails++))
+	fi
+done
+echo $heads
+echo $tails
+echo $toss
+percentageOfHeads=$(echo "scale=2; $heads/$toss*100" | bc)
+percentageOfTails=$(echo "scale=2; $tails/$toss*100" | bc)
+echo
